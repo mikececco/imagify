c01ee593385a59630ae676e117f82c1eea41438b
Mike Cecconello <mikececco2000@gmail.com>
2024-09-11 21:48:38 +0200

    sidebar displayed with icons and clickable with Link

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,12 @@
+import SideBar from '@/components/shared/SideBar'
 import React from 'react'
 
 function Layout({children}: {children: React.ReactNode}) {
   return (
     <main className='root'>
+      {/* Sidebar */}
+      <SideBar />
+      {/* MobileNav */}
       <div className='root-container'>
         <div className='wrapper'>
           {children}
