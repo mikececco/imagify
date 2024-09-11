64fb39110a564422fa9d25e28fcaabcb2b23923a
Mike Cecconello <mikececco2000@gmail.com>
2024-09-11 21:44:32 +0200

    sidebar displayed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='flex justify-between'>
+      <p>
+        Home
+      </p>
+      <SignedIn>
+        {/* Mount the UserButton component */}
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        {/* Signed out users get sign in button */}
+        <SignInButton />
+      </SignedOut>
+    </div>
   )
 }
 
