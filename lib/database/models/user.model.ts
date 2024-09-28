cded67482d8ba7fc30c01231baad3faedf3aa13a
Mike Cecconello <mikececco2000@gmail.com>
2024-09-28 21:50:20 +0200

    Sync with MongoDb and Clerk working

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,33 +1,41 @@
-// clerkId, email, username, photo, firstname, lastName, plaId, creditBalance
-
-import { model, models, Schema } from "mongoose";
-
-export interface User extends Document {
-  clerkId: string;
-  email: string;
-  username: string;
-  photo: string;
-  firstName: string;
-  lastName: string;
-  plaId: string;
-  creditBalance: number;
-  createdAt: Date;
-  updatedAt: Date;
-}
+import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-  clerkId: { type: String, required: true, unique: true },
-  email: { type: String, required: true },
-  username: { type: String, required: true },
-  photo: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  plaId: { type: String, required: true },
-  creditBalance: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  planId: {
+    type: Number,
+    default: 1,
+  },
+  creditBalance: {
+    type: Number,
+    default: 10,
+  },
 });
 
-const User = models?.User || model('User', UserSchema);
+const User = models?.User || model("User", UserSchema);
 
 export default User;
