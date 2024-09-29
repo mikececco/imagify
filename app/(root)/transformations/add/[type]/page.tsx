e3415efcb5f97695104da8fcc72529174d19deb5
Mike Cecconello <mikececco2000@gmail.com>
2024-09-29 18:55:45 +0200

    1:46:46 / 4:42:54
    header based on [type] coming from list of different pages

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import Header from '@/components/shared/Header'
+import { transformationTypes } from '@/constants'
 
-function AddTransformationTypePage() {
+function AddTransformationTypePage({params: {type}} : SearchParamProps) {
+
+  const transformation = transformationTypes[type];
+
   return (
-    <div>AddTransformationTypePage</div>
+    <Header
+      title={transformation.title}
+      subtitle={transformation.subTitle}
+    />
   )
 }
 
